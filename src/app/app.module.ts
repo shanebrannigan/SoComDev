302308e422d543e768a4bb2924c76b08501c4e83
James <james.cole01@liberty-it.co.uk>
2019-01-29 14:37:47 +0000

    James EventSignUp with GuestName

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HomeLoginComponent } from './home-login/home-login.component';
 import { HomeSignupComponent } from './home-signup/home-signup.component';
 import { HomeEventDisplayComponent } from './home-event-display/home-event-display.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {EventSignupComponent} from './event-signup/event-signup.component';
+import {AdminEventCreateComponent} from './admin-event-create/admin-event-create.component';
 
 
 @NgModule({

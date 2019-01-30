4b70e99311dd332d4a7123b43cee1d41c71a77dc
James <james.cole01@liberty-it.co.uk>
2019-01-30 11:24:50 +0000

    initial commit
    # Conflicts:
    #	src/app/app.component.html
    #	src/app/app.component.ts
    #	src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HomeLoginComponent } from './home-login/home-login.component';
 import { HomeSignupComponent } from './home-signup/home-signup.component';

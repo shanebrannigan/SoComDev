2592decb3adf082beff9e8afc04be1874b571c89
James <james.cole01@liberty-it.co.uk>
2019-01-30 10:00:53 +0000

    James EventSignUp with GuestName

--- src/app/home-event-display/home-event-display.component.ts
+++ src/app/home-event-display/home-event-display.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {EventServiceHttp} from '../services/event-service-http';
 import {Event} from '../model/event';
@@ -11,6 +11,8 @@ import {Event} from '../model/event';
   providers: [EventServiceHttp]
 })
 export class HomeEventDisplayComponent implements OnInit {
+  @Output() showEventSignUpForm = new EventEmitter<boolean>();
+  @Output() showSignUp = false;
 
   eventToDisplay: Event;
 
@@ -20,5 +22,12 @@ export class HomeEventDisplayComponent implements OnInit {
   ngOnInit() {
     // get the events, display them
     this.eventToDisplay = this.eventService.getEvent();
+  }
+
+  onClickIWantToGo() {
+    console.log('show events!');
+    this.showSignUp = true;
+    console.log(this.showSignUp);
+    this.showEventSignUpForm.emit(this.showSignUp);
   }
 }
